6212c1446ac27ee186054230ff07a95dcc5847c6
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 14:05:43 -0800

    Added another header to CORS arguments

--- my_app_file.py
+++ my_app_file.py
@@ -6,10 +6,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, 
      origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"],
-     allow_headers=["Content-Type","multipart/form-data","Authorization"],
+     allow_headers=["Content-Type","multipart/form-data","Authorization", "Access-Control-Allow-Origin"],
      supports_credentials=True)
 @app.route('/', methods = ["GET","POST", "OPTIONS"])
-@cross_origin(origin = "*", headers = ["Content-Type", "Authorization"])
+# @cross_origin(origin = "*", headers = ["Content-Type", ])
 def hello():
     if flask.request.method == "OPTIONS":
         return _build_cors_preflight_response()
