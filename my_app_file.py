d3b8670392a802c4304f9a7d3e119916138c0e89
Mason Acevedo <macevedo@hmc.edu>
2023-12-28 20:43:22 -0800

    Added yet another site to CORS

--- my_app_file.py
+++ my_app_file.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com"])
+CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com"])
 @app.route('/')
 def hello():
     return flask.jsonify(prediction = ("bird"), probability = (0.69))
