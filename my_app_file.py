5be2a5b01bb90f2d2aab3f516976774d0c1d8af3
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 13:26:57 -0800

    Added preflight response?

--- my_app_file.py
+++ my_app_file.py
@@ -4,7 +4,19 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
-@app.route('/', methods = ["GET","POST"])
+CORS(app, 
+     origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"],
+     allow_headers=["Content-Type","multipart/form-data","Authorization"],
+     supports_credentials=True)
+@app.route('/', methods = ["GET","POST", "OPTIONS"])
 def hello():
+    if flask.request.method == "OPTIONS":
+        return _build_cors_preflight_response()
     return flask.jsonify(prediction = ("bird"), probability = (0.69))
+
+def _build_cors_preflight_response():
+    response = flask.make_response()
+    response.headers.add("Access-Control-Allow-Origin","https://www.masonacevedo.com")
+    response.headers.add("Access-Control-Allow-Headers", '*')
+    response.headers.add("Access-Control-Allow-Methods", '*')
+    return response
