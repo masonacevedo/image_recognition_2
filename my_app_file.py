e7ce35c2c664a1fde73a95ed132e9b7b76c512c7
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 21:50:38 -0800

    Added options back

--- my_app_file.py
+++ my_app_file.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
 # CORS(app)
-@app.route('/', methods = ["POST"])
+@app.route('/', methods = ["POST", "OPTIONS"])
 def hello():
     response = flask.jsonify(prediction = ("bird"), probability = (0.69))
     response.headers['Access-Control-Allow-Origin'] = "*"
