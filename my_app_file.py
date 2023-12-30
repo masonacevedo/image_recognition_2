74b6cb311ee2cbbb4f8a2adb2e71b06a7c23a637
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 22:26:11 -0800

    Added a print statement

--- my_app_file.py
+++ my_app_file.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
 # CORS(app)
-@app.route('/', methods = ["POST", "OPTIONS"])
+@app.route('/', methods = ["GET", "POST", "OPTIONS"])
 def hello():
+    print("I can see a statement")
     response = flask.jsonify(prediction = ("bird"), probability = (0.69))
     return response
