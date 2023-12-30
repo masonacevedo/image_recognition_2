980204748c65bd8108139c2a8ee98eaf0e08ce88
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 21:38:17 -0800

    Added more headers?

--- my_app_file.py
+++ my_app_file.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
 # CORS(app)
-@app.route('/', methods = ["GET","POST"])
+@app.route('/', methods = ["POST"])
 def hello():
     response = flask.jsonify(prediction = ("bird"), probability = (0.69))
     response.headers['Access-Control-Allow-Origin'] = "*"
