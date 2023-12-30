4a9eaf7953728d19ad0f1a35a85e2a23dc7775d5
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 18:03:46 -0800

    Trying to add a header to the response

--- my_app_file.py
+++ my_app_file.py
@@ -7,4 +7,6 @@ app = Flask(__name__)
 CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
 @app.route('/', methods = ["GET","POST"])
 def hello():
-    return flask.jsonify(prediction = ("bird"), probability = (0.69))
+    response = flask.jsonify(prediction = ("bird"), probability = (0.69))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
