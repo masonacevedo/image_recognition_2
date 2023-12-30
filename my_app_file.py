434fc77a4e35d1e4e674a20403cbd115b409d3c4
Mason Acevedo <macevedo@hmc.edu>
2023-12-29 19:50:35 -0800

    Enable all websites to make requests (for testing purposes!)

--- my_app_file.py
+++ my_app_file.py
@@ -4,7 +4,8 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
+# CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www.masonacevedo.com", "www.masonacevedo.com"])
+CORS(app)
 @app.route('/', methods = ["GET","POST"])
 def hello():
     response = flask.jsonify(prediction = ("bird"), probability = (0.69))
