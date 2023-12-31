5ffc61b7421a7eb8ad4deac3ff2f931d949949cf
Mason Acevedo <macevedo@hmc.edu>
2023-12-30 23:12:53 -0800

    Added actual image recognition

--- my_app_file.py
+++ my_app_file.py
@@ -4,6 +4,9 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request
 from PIL import Image
+from fastai.vision.all import *
+import pickle
+import pathlib
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -25,10 +28,8 @@ def hello():
         web_image_plugin_obj = Image.open(image)
         web_image_plugin_obj.save("image.jpg")
         
-
-        response_dict = {"prediction":"bird", 
-                         "probability":12}
-        response = flask.jsonify(response_dict)
+        results_dict = classifyImage(web_image_plugin_obj)
+        response = flask.jsonify(results_dict)
         return response
     
     elif request.method == "GET":
@@ -38,5 +39,32 @@ def hello():
     else:
         raise Exception("It should be impossible to get here!")
 
-if __name__ == "__main__":
-    app.run(debug= True)
+def classifyImage(web_image_plugin_obj):
+    fastai_image = PILImage.create(web_image_plugin_obj)
+    resized_image = Resize(192, method = 'squish')(fastai_image)
+    mean = [0.4771, 0.4596, 0.4162]
+    stddev = [0.2288, 0.2205, 0.2198]
+
+    normalized_resized_image = Normalize.from_stats(mean, stddev)(resized_image)
+    print("Got to the model loading part...")
+    # fast_ai_model_with_normalization_with_architecture.pkl
+    
+    
+    posix_backup = pathlib.PosixPath
+    try:
+        pathlib.PosixPath = pathlib.WindowsPath
+        model_path = pathlib.Path("fast_ai_model_with_normalization_with_architecture.pkl")
+    
+        model = load_learner(model_path)
+    finally:
+        pathlib.PosixPath = posix_backup
+
+
+
+    prediction = model.predict(normalized_resized_image)
+    print("prediction[0]:", prediction[0])
+    print("prediction[2].max():", prediction[2].max())
+    return {"prediction": prediction[0], "probability": float(prediction[2].max())}
+
+# if __name__ == "__main__":
+#     app.run(debug= True)
