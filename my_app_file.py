390a68d985bf1643efcbae86f63401035671af98
Mason Acevedo <macevedo@hmc.edu>
2023-12-30 22:10:54 -0800

    Developing actual image recognition

--- my_app_file.py
+++ my_app_file.py
@@ -3,6 +3,7 @@ import flask
 from flask import Flask
 from flask_cors import CORS
 from flask import request
+from PIL import Image
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -14,11 +15,17 @@ CORS(app, origins= ["masonacevedo.com", "https://masonacevedo.com", "https://www
 @app.route('/', methods = ["GET", "POST"])
 def hello():
     if request.method == "POST":
+        print("request.files:", request.files)
+        # print("file:", file)
         image = request.files.get("user_image")
-        print("\n\n\ntype(image):", type(image),"\n\n\n\n")
+        print("image:", image,"\n\n\n\n")
         if image is None:
             return flask.jsonify(error = "No image provided! Please upload an image!"), 400
         
+        web_image_plugin_obj = Image.open(image)
+        web_image_plugin_obj.save("image.jpg")
+        
+
         response_dict = {"prediction":"bird", 
                          "probability":12}
         response = flask.jsonify(response_dict)
@@ -30,3 +37,6 @@ def hello():
         return response
     else:
         raise Exception("It should be impossible to get here!")
+
+if __name__ == "__main__":
+    app.run(debug= True)
